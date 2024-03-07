be3d6601d683f3dc48a6fc4925fdd01c6351cdb0
hanos <zhuhanos@gmail.com>
2024-03-07 11:14:49 +0800

    添加项目文件。

---
+++ GalleryApp/Models/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalleryApp.Models
+{
+    public class Photo
+    {
+        public string Filename { get; set; }
+        public byte[] Bytes { get; set; }
+    }
+}
